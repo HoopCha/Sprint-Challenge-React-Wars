9a0438fcaf214bcb8e3f22bb99771835e5f59bd5
Chase Hooper <43388614+HoopCha@users.noreply.github.com>
2019-10-11 10:00:11 -0600

    Added Search

--- starwars/src/components/StarWarsPage.js
+++ starwars/src/components/StarWarsPage.js
@@ -13,22 +13,39 @@ justify-content:space-evenly;
 export default function StarWarsPage() {
 
     const [people, setPeople] = useState ([])
+    const [query, setQuery] = useState("");
 
 useEffect(() => {
     axios  
         .get(`https://swapi.co/api/people`)
         .then(response => {
-            const people = response.data.results;
+            const people = response.data.results.filter(character =>
+                character.name.toLowerCase().includes(query.toLowerCase())
+              );;
             setPeople(people)
-            console.log(people);
+            console.log(response);
         })
         .catch(error => {
             console.log("These are not the droids your looking for", error);
         });
-}, []);
-
+}, [query]);
+const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
 return (
     <div>
+        <form className="search">
+        <input
+          type="text" 
+          onChange={handleInputChange}
+          value={query}
+          name="name"
+          tabIndex="0"
+          className="prompt search-name"
+          placeholder="search by name"
+          autoComplete="off"
+        />
+      </form>
     <Main>
       {people.map((data, index )=> {
           return (
